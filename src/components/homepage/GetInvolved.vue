170af556d7a5d0b7d9742b1e5e0c5a451b6af94c
Rishan <79592017+gspxrk@users.noreply.github.com>
2024-09-08 19:53:33 +0530

    Update GetInvolved.vue

--- src/components/homepage/GetInvolved.vue
+++ src/components/homepage/GetInvolved.vue
@@ -1,110 +1,87 @@
 <template>
-    <section class="get-involved" id="getInvolved">
-       <!-- JUNIOR DEVELOPERS -->
-       <div class="get-involved__column">
-         <div class="badge">Get Involved</div>
+    <section class="creative-collaborations" id="creativeCollaborations">
+       <!-- CREATIVE COLLABORATORS -->
+       <div class="creative-collaborations__column">
+         <div class="badge">Join Us</div>
          <div class="column-title">
-           <h3>Junior Developers</h3>
-         </div>
-         <div class="column-price">
-           <p>
-             <strong> £0</strong>
-             <span> / membership</span>
-           </p>
+           <h3>Creative Collaborators</h3>
          </div>
          <div class="column-information">
            <ul>
-             <li><strong>Request</strong> skill appropriate reviews &amp; feedback</li>
-             <li><strong>Meet </strong> new people and practice public speaking</li>
-             <li><strong>Participate</strong> in workshops &amp; hackathon events</li>
-             <li><strong>Get familiar</strong> with tools like Jira </li>
+             <li><strong>Collaborate</strong> on art and design projects</li>
+             <li><strong>Participate</strong> in community challenges and contests</li>
+             <li><strong>Showcase</strong> your work in Apsara exhibitions</li>
+             <li><strong>Join</strong> workshops and creative meetups</li>
            </ul>
          </div>
          <div class="column-button">
-           <a href="/member-application-form">Become a member</a>
+           <a href="/collaboration-form">Get Involved</a>
          </div>
        </div>
  
-       <!-- SKATEBOY IMAGE -->
-       <div class="get-involved__column skateboy">
-         <img src="../../assets/images/skateboy.png" alt="a skateboy in an orange coloured tshirt enjoying his ride">
+       <!-- INSPIRATIONAL IMAGE -->
+       <div class="creative-collaborations__column inspiration">
+         <img src="../../assets/images/inspiration.png" alt="A vibrant collection of artworks and creative projects">
          <br>
        </div>
  
-       <!-- SENIOR DEVELOPERS AND MENTORS -->
-       <div class="get-involved__column">
-         <div class="badge">Get Involved</div>
+       <!-- COMMUNITY LEADERS -->
+       <div class="creative-collaborations__column">
+         <div class="badge">Lead the Way</div>
          <div class="column-title">
-           <h3>Mentors &amp; Team Leads</h3>
-         </div>
-         <div class="column-price">
-           <p>
-             <strong> £0</strong>
-             <span> / membership</span>
-           </p>
+           <h3>Community Leaders</h3>
          </div>
          <div class="column-information">
            <ul>
-             <li>
-               <strong>Help</strong> others with skill appropriate feedback
-             </li>
-             <li>
-               <strong>Practice</strong> planning and organising projects
-             </li>
-             <li><strong>Lead</strong> monthly workshops &amp; hackathon events</li>
-             <li><strong>Introduce</strong> use of tools like Jira </li>
+             <li><strong>Lead</strong> creative workshops and webinars</li>
+             <li><strong>Mentor</strong> emerging artists and creators</li>
+             <li><strong>Organize</strong> community-driven art events</li>
+             <li><strong>Innovate</strong> with new artistic techniques and tools</li>
            </ul>
          </div>
          <div class="column-button">
-           <a href="/mentor-application-form">Become a mentor</a>
+           <a href="/leadership-form">Become a Leader</a>
          </div>
        </div>
      </section>
  </template>
  
- 
  <script>
  export default {
-     // eslint-disable-next-line
-     name: 'GetInvolved'
+     name: 'CreativeCollaborations'
  }
  </script>
  
- 
  <style lang="scss" scoped>
-
-  .skateboy {
+ .inspiration {
     display: flex;
     align-items: center;
     justify-content: center;
 
       img {
         width: 250px;
-        height: 450px;
+        height: auto;
       }
 
         @media (max-width: 600px) {
           img {
               width: 80%;
-              height: unset;
           }
         }
   }
-     .get-involved {
+     .creative-collaborations {
          display: flex;
          justify-content: space-evenly;
          align-items: center;
          min-height: 900px;
          width: 100%;
          flex-wrap: wrap;
-         min-height: 900px;
          background: #f5f4f4;
      }
  
-     .get-involved__column {
+     .creative-collaborations__column {
          max-width: 400px;
      }
- 
  
      .badge {
          background: #ffc22b;
@@ -135,31 +112,6 @@
          text-align: center;
          margin-bottom: 40px;
      }
- 
- 
- .column-price {
-     display: flex;
-     justify-content: center;
-     align-items: center;
- 
-     strong {
-         font-style: normal;
-         font-weight: normal;
-         font-size: 44px;
-         line-height: 25px;
-         letter-spacing: 0.04em;
-         color: #21243d;
-     }
- 
-     span {
-         font-style: normal;
-         font-weight: normal;
-         font-size: 22px;
-         line-height: 25px;
-         letter-spacing: 0.04em;
-         color: #21243d;
-     }
- }
  
  .column-information {
      display: flex;
@@ -199,7 +151,7 @@
  }
  
  @media (max-width: 980px) {
-     .get-involved__column {
+     .creative-collaborations__column {
          margin-top: 50px;
          margin-bottom: 50px;
  
@@ -225,7 +177,5 @@
              padding-inline-start: 0;
          }
      }
- 
-     
  }
  </style>
